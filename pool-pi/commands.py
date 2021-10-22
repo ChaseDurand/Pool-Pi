ae87c30fbae95e71b25797738006a7b5b4ed3451
ChaseDurand <chase.durand@gmail.com>
2021-10-22 19:51:41 +0100

    Modified commands.py to remove binascii.

--- pool-pi/commands.py
+++ pool-pi/commands.py
@@ -1,7 +1,5 @@
-import binascii
-
-DLE = binascii.unhexlify('10')
-STX = binascii.unhexlify('02')
-ETX = binascii.unhexlify('03')
+DLE = b'\x10'
+STX = b'\x02'
+ETX = b'\x03'
 
 KEEP_ALIVE = (b'\x10\x02\x01\x01\x00\x14\x10\x03', "Keep Alive")
