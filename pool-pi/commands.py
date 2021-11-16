913206692833ef6537938edffe83027270ce17c4
ChaseDurand <chase.durand@gmail.com>
2021-11-15 23:10:33 -0600

    Checking if changing DISPLAY_AIRTEMP to non-encoded version then encoding still functions as expected to improve code readability.

--- pool-pi/commands.py
+++ pool-pi/commands.py
@@ -8,7 +8,14 @@ NON_KEEP_ALIVE = ('', '')
 
 FRAME_UPDATE_LEDS = (b'\x01\x02', "FRAME_UPDATE_LEDS")
 FRAME_UPDATE_DISPLAY = (b'\x01\x03', "FRAME_UPDATE_DISPLAY")
-DISPLAY_AIRTEMP = (b'\x41\x69\x72\x20\x54\x65\x6D\x70')  #'Air Temp'
+# DISPLAY_AIRTEMP = (b'\x41\x69\x72\x20\x54\x65\x6D\x70')  #'Air Temp'
+DISPLAY_AIRTEMP = 'Air Temp'.encode('utf-8')
+DISPLAY_POOLTEMP = (b'\x50\x6F\x6F\x6C\x20\x54\x65\x6D\x70')  #'Pool Temp'
+DISPLAY_GASHEATER = (b'\x47\x61\x73\x20\x48\x65\x61\x74\x65\x72'
+                     )  #'Gas Heater'
+DISPLAY_CHLORINATOR_PERCENT = (
+    b'\x50\x6F\x6F\x6C\x20\x43\x68\x6C\x6F\x72\x69\x6E\x61\x74\x6F\x72'
+)  #'Pool Chlorinator'
 '''
 regular display updates:
 Gas heater status
