e08ef4abcc7296f15b9ff473387c65c0d183d446
ChaseDurand <chase.durand@gmail.com>
2021-11-26 22:57:07 -0600

    Fixed missing ) in LED_1

--- pool-pi/commands.py
+++ pool-pi/commands.py
@@ -17,16 +17,9 @@ DISPLAY_SALT_LEVEL = 'Salt Level'.encode('utf-8')
 DISPLAY_DATE = 'day'.encode('utf-8')
 DISPLAY_CHECK = 'Check System'.encode('utf-8')
 
-LED_1 = [
-    (1 << 0, 'Heater 1'),
-    (1 << 1, 'Valve 3'),
-    (1 << 2, 'Check System'),
-    (1 << 3, 'Pool',
-    (1 << 4, 'Spa'),
-    (1 << 5, 'Filter'),
-    (1 << 6, 'Lights'),
-    (1 << 7, 'AUX 1')
-]
+LED_1 = [(1 << 0, 'Heater 1'), (1 << 1, 'Valve 3'), (1 << 2, 'Check System'),
+         (1 << 3, 'Pool'), (1 << 4, 'Spa'), (1 << 5, 'Filter'),
+         (1 << 6, 'Lights'), (1 << 7, 'AUX 1')]
 # LEDs 2
 LED_AUX2 = 1 << 0
 LED_SERVICE = 1 << 1
