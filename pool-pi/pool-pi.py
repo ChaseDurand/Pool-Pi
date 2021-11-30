1f9ef8c110b6ab74f219e128854ed500a981e6d3
ChaseDurand <chase.durand@gmail.com>
2021-11-29 21:06:18 -0600

    Miguel Grinberg's example

--- pool-pi/pool-pi.py
+++ pool-pi/pool-pi.py
@@ -1,26 +1,44 @@
 import serial
 from gpiozero import LED
 from commands import *
-from flask import Flask, render_template, session, copy_current_request_context
-from flask_socketio import SocketIO, emit, disconnect
-import threading
-import datetime
+from flask import Flask, render_template, session, request, \
+    copy_current_request_context
+from flask_socketio import SocketIO, emit, join_room, leave_room, \
+    close_room, rooms, disconnect
+from threading import Lock
+from threading import Thread
 
+# Set this variable to "threading", "eventlet" or "gevent" to test the
+# different async modes, or leave it set to None for the application to choose
+# the best option based on installed packages.
 async_mode = None
+
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
-socket_ = SocketIO(app, async_mode=async_mode)
+socketio = SocketIO(app, async_mode=async_mode)
 thread = None
-thread_lock = threading.Lock()
+thread_lock = Lock()
+
+
+def background_thread():
+    """Example of how to send server generated events to clients."""
+    count = 0
+    while True:
+        socketio.sleep(10)
+        count += 1
+        socketio.emit('my_response', {
+            'data': 'Server generated event',
+            'count': count
+        })
 
 
 @app.route('/')
 def index():
-    return render_template('index.html', async_mode=socket_.async_mode)
+    return render_template('index.html', async_mode=socketio.async_mode)
 
 
-@socket_.on('my_event', namespace='/test')
-def test_message(message):
+@socketio.event
+def my_event(message):
     session['receive_count'] = session.get('receive_count', 0) + 1
     emit('my_response', {
         'data': message['data'],
@@ -28,8 +46,8 @@ def test_message(message):
     })
 
 
-@socket_.on('my_broadcast_event', namespace='/test')
-def test_broadcast_message(message):
+@socketio.event
+def my_broadcast_event(message):
     session['receive_count'] = session.get('receive_count', 0) + 1
     emit('my_response', {
         'data': message['data'],
@@ -38,18 +56,83 @@ def test_broadcast_message(message):
          broadcast=True)
 
 
-@socket_.on('disconnect_request', namespace='/test')
+@socketio.event
+def join(message):
+    join_room(message['room'])
+    session['receive_count'] = session.get('receive_count', 0) + 1
+    emit(
+        'my_response', {
+            'data': 'In rooms: ' + ', '.join(rooms()),
+            'count': session['receive_count']
+        })
+
+
+@socketio.event
+def leave(message):
+    leave_room(message['room'])
+    session['receive_count'] = session.get('receive_count', 0) + 1
+    emit(
+        'my_response', {
+            'data': 'In rooms: ' + ', '.join(rooms()),
+            'count': session['receive_count']
+        })
+
+
+@socketio.on('close_room')
+def on_close_room(message):
+    session['receive_count'] = session.get('receive_count', 0) + 1
+    emit('my_response', {
+        'data': 'Room ' + message['room'] + ' is closing.',
+        'count': session['receive_count']
+    },
+         to=message['room'])
+    close_room(message['room'])
+
+
+@socketio.event
+def my_room_event(message):
+    session['receive_count'] = session.get('receive_count', 0) + 1
+    emit('my_response', {
+        'data': message['data'],
+        'count': session['receive_count']
+    },
+         to=message['room'])
+
+
+@socketio.event
 def disconnect_request():
     @copy_current_request_context
     def can_disconnect():
         disconnect()
 
     session['receive_count'] = session.get('receive_count', 0) + 1
+    # for this emit we use a callback function
+    # when the callback function is invoked we know that the message has been
+    # received and it is safe to disconnect
     emit('my_response', {
         'data': 'Disconnected!',
         'count': session['receive_count']
     },
          callback=can_disconnect)
+
+
+@socketio.event
+def my_ping():
+    emit('my_pong')
+
+
+@socketio.event
+def connect():
+    global thread
+    with thread_lock:
+        if thread is None:
+            thread = socketio.start_background_task(background_thread)
+    emit('my_response', {'data': 'Connected', 'count': 0})
+
+
+@socketio.on('disconnect')
+def test_disconnect():
+    print('Client disconnected', request.sid)
 
 
 # async_mode = None
@@ -300,6 +383,6 @@ def main():
 
 
 if __name__ == '__main__':
-    threading.Thread(
-        target=lambda: socket_.run(app, debug=False, host='0.0.0.0')).start()
-    threading.Thread(target=main).start()
+    Thread(
+        target=lambda: socketio.run(app, debug=False, host='0.0.0.0')).start()
+    Thread(target=main).start()
