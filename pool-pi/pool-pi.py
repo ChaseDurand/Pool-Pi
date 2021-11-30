fe3b2a7ac200279106729f246e12a8fa57b83311
ChaseDurand <chase.durand@gmail.com>
2021-11-29 23:59:59 -0600

    Cleaned up some unused forms.

--- pool-pi/pool-pi.py
+++ pool-pi/pool-pi.py
@@ -61,73 +61,8 @@ def my_broadcast_event(message):
 
 
 @socketio.event
-def join(message):
-    join_room(message['room'])
-    session['receive_count'] = session.get('receive_count', 0) + 1
-    emit(
-        'my_response', {
-            'data': 'In rooms: ' + ', '.join(rooms()),
-            'count': session['receive_count']
-        })
-
-
-@socketio.event
-def leave(message):
-    leave_room(message['room'])
-    session['receive_count'] = session.get('receive_count', 0) + 1
-    emit(
-        'my_response', {
-            'data': 'In rooms: ' + ', '.join(rooms()),
-            'count': session['receive_count']
-        })
-
-
-@socketio.on('close_room')
-def on_close_room(message):
-    session['receive_count'] = session.get('receive_count', 0) + 1
-    emit('my_response', {
-        'data': 'Room ' + message['room'] + ' is closing.',
-        'count': session['receive_count']
-    },
-         to=message['room'])
-    close_room(message['room'])
-
-
-@socketio.event
-def my_room_event(message):
-    session['receive_count'] = session.get('receive_count', 0) + 1
-    emit('my_response', {
-        'data': message['data'],
-        'count': session['receive_count']
-    },
-         to=message['room'])
-
-
-@socketio.event
-def disconnect_request():
-    @copy_current_request_context
-    def can_disconnect():
-        disconnect()
-
-    session['receive_count'] = session.get('receive_count', 0) + 1
-    # for this emit we use a callback function
-    # when the callback function is invoked we know that the message has been
-    # received and it is safe to disconnect
-    emit('my_response', {
-        'data': 'Disconnected!',
-        'count': session['receive_count']
-    },
-         callback=can_disconnect)
-
-
-@socketio.event
 def my_ping():
     emit('my_pong')
-
-
-# @socketio.event
-# def salinity():
-#     emit('salinity')
 
 
 @socketio.event
@@ -143,19 +78,6 @@ def connect():
 def test_disconnect():
     print('Client disconnected', request.sid)
 
-
-# async_mode = None
-# app = Flask(__name__)
-# socket_ = SocketIO(app, async_mode=async_mode)
-
-# @app.route("/")
-# def index():
-#     now = datetime.datetime.now()
-#     timeString = now.strftime("%Y-%m-%d %H:%M")
-#     templateData = {'title': 'Hello!', 'time': timeString}
-#     return render_template('index.html',
-#                            **templateData,
-#                            sync_mode=socket_.async_mode)
 
 buffer = bytearray()
 buffer_full = False
