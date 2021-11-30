b4fb3243c30daabc91d6110a8139e92d86590d7c
ChaseDurand <chase.durand@gmail.com>
2021-11-29 22:13:39 -0600

    Moved salt_level log to updateModel()

--- pool-pi/pool-pi.py
+++ pool-pi/pool-pi.py
@@ -367,7 +367,7 @@ def updateModel():
     global flag_data_changed
     if not flag_data_changed:
         return
-
+    socketio.emit('my_response', {'data': 'Salt Level', 'count': salt_level})
     flag_data_changed = False
     #TODO
     return
