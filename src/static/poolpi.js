9d066fb5e750e6c6a7961b82e4d324d56a7f991b
ChaseDurand <chase.durand@gmail.com>
2022-01-15 11:39:05 -0600

    Updated sending_message flag name

--- src/static/poolpi.js
+++ src/static/poolpi.js
@@ -51,8 +51,8 @@ $(document).ready(function () {
         // Parse every item in json message
         for (var i = 0, len = Object.keys(msgObj).length; i < len; ++i) {
             attributeName = Object.keys(msgObj)[i];
-            // Parse sendingMessage flag (no version)
-            if (attributeName == 'sendingMessage') {
+            // Parse sending_message flag (no version)
+            if (attributeName == 'sending_message') {
                 if (msgObj[attributeName] == true) {
                     document.getElementsByClassName('overlay')[0].style.display = 'flex'
                 }
