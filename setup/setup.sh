2f603d6e894d7f93bd7e3e68445979b4d8546cc9
Chase Durand <chase.durand@gmail.com>
2022-12-27 09:00:35 -0600

    Corrected setup.sh. Changed service directory from lib to etc. Corrected systemctl enable target from example code to correct service. Added --now flag and removed reboot to start service without reboot.

--- setup/setup.sh
+++ setup/setup.sh
@@ -3,11 +3,8 @@ echo "Setting up pool-pi."
 echo "Installing required python packages."
 pip3 install -r /home/pi/Pool-Pi/setup/requirements.txt
 echo "Configuring systemd."
-mv /home/pi/Pool-Pi/setup/poolpi.service /lib/systemd/system/poolpi.service
-chmod 644 /lib/systemd/system/poolpi.service
+cp /home/pi/Pool-Pi/setup/poolpi.service /etc/systemd/system/poolpi.service
+chmod 644 /etc/systemd/system/poolpi.service
 systemctl daemon-reload
-systemctl enable myscript.service
-echo "Setup complete."
-echo "Rebooting in 10 seconds."
-sleep 10
-reboot
+systemctl enable --now poolpi.service
+echo "Setup script complete."
