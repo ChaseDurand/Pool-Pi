e34b56e8dcab99a110fd2b139b5e22e253b6e1ec
Chase Durand <chase.durand@gmail.com>
2023-11-24 15:59:42 -0600

    Removed unnecessary root use in setup.

--- setup/setup.sh
+++ setup/setup.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
-# This assumes setup.sh is located in */Pool-Pi/setup
-BASEDIR=$(cd "$(dirname "${BASH_SOURCE[0]}")"; cd .. ; pwd -P)
-echo "Setting up pool-pi."
-echo "Creating venv."
-pip3 install virtualenv
-virtualenv ${BASEDIR}"/src/.venv"
-source ${BASEDIR}"/src/.venv/bin/activate"
-echo "Installing required python packages."
-pip3 install -r /home/pi/Pool-Pi/setup/requirements.txt
-echo "Configuring systemd."
-cp /home/pi/Pool-Pi/setup/poolpi.service /etc/systemd/system/poolpi.service
+sudo -u $SUDO_UID BASEDIR=$(cd "$(dirname "${BASH_SOURCE[0]}")"; cd .. ; pwd -P)
+sudo -u $SUDO_UID echo "Setting up pool-pi."
+sudo -u $SUDO_UID echo "Creating venv."
+sudo -u $SUDO_UID pip3 install virtualenv
+sudo -u $SUDO_UID virtualenv ${BASEDIR}"/src/.venv"
+sudo -u $SUDO_UID source ${BASEDIR}"/src/.venv/bin/activate"
+sudo -u $SUDO_UID echo "Installing required python packages."
+sudo -u $SUDO_UID pip3 install -r /home/pi/Pool-Pi/setup/requirements.txt
+sudo -u $SUDO_UID echo "Configuring systemd."
+cp ${BASEDIR}/setup/poolpi.service /etc/systemd/system/poolpi.service
 chmod 644 /etc/systemd/system/poolpi.service
 systemctl daemon-reload
 systemctl enable redis-server
 systemctl enable --now poolpi.service
-echo "Setup script complete."
+sudo -u $SUDO_UID echo "Setup script complete."
